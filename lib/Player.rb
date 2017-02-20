332685bc06fb230b388a68b0f26eda7f5ea9819a
Brant pastore <brant1pastore@gmail.com>
2017-02-20 15:45:45 -0600

    The player selects who goes first

---
+++ lib/Player.rb
@@ -0,0 +1,6 @@
+class Player
+	def SelectionScreen()
+		puts "\nWho goes first? (1 For player, 2 for Computer):"
+		return gets.chomp
+	end
+end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,8 +1,24 @@
-# list of rules
+require 'player'
+
+# Rules
 # 3 in a row = win
 # User vs Computer
 describe 'Tic Taco Toe' do
-	it 'is a Player verses computer game' do
-		#initialize the bot
+	it 'has the player select who goes first' do
+		state = 1
+		nPlayer = Player.new
+		res = nPlayer.SelectionScreen()
+
+		if(res == 1)
+			it 'has the player select their next position' do
+				puts "Its your turn: Select the your position based on the number shown on your screen"
+				sel = gets.chomp
+				nBoard.move(1, sel)
+			end
+		elsif (res == 2)
+			it 'has the computer calculate its next move' do
+				nBoard.move(2, 3)
+			end
+		end
 	end
 end
